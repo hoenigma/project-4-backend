3c9b93de041211c729e814d8b0ac2b3f0aac0578
Matthew <mhoenig35@gmail.com>
2024-04-11 19:01:31 +0100

    got app.py working in insomnia (Hello function working). Set up files and folders

---
+++ app.py
@@ -0,0 +1,34 @@
+# save this as app.py
+from flask import Flask
+
+# import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+# import marshmallow
+from flask_marshmallow import Marshmallow
+
+# import bcrypt
+from flask_bcrypt import Bcrypt
+
+# import the uri
+from config.environment import db_URI
+
+app = Flask(__name__)
+
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return "Hello, World!"
+
+
+# Added this configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = db_URI
+
+#  Instatiate the SQLAlchemy class
+db = SQLAlchemy(app)
+
+# Instantiate Marshmalllow to tell it about Flask (like the above fro SQLA)
+marsh = Marshmallow(app)
+
+# ! Create bcrypt plugin, tell it about flask
+bcrypt = Bcrypt(app)
---
+++ config/environment.py
@@ -0,0 +1,4 @@
+#  URI we use to talk to postgres
+db_URI = "postgresql://localhost:5432/conservation_db"
+# Secret for JWT
+SECRET = "duckmariotinalego"
